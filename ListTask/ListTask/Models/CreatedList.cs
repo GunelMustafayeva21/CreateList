4f3188817a1f9ed332d363daee7e7bdf30e3f641
GunelMustafayeva21 <74umx1v@code.edu.az>
2022-04-07 04:35:15 +0300

    Create List with Methods

---
+++ ListTask/ListTask/Models/CreatedList.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListTask
+{
+    internal class CreatedList<T>
+    {
+        private T[] array = new T[0];
+        private int count;
+        private int capacity = 0;
+        private int length = 0;
+
+
+        public void Add(T element)
+        {
+            if (Resizable())
+                if (capacity == 0)
+                {
+                    capacity += 2;
+                }
+                else
+                {
+                    capacity *= 2;
+                }
+            Array.Resize(ref array, capacity);
+
+            array[length] = element;
+            length++;
+
+        }
+        public void AddItems(params T[] elements)
+        {
+            foreach (var item in elements)
+            {
+                Add(item);
+            }
+        }
+        private bool Resizable()
+        {
+            if (length == capacity)
+                return true;
+            return false;
+        }
+        public void Capacity()
+        {
+            Console.WriteLine(capacity);
+        }
+        public void Count()
+        {
+            count = length;
+            Console.WriteLine(count);
+        }
+
+        public void Clear()
+        {
+            array = new T[0];
+            length = 0;
+            count = 0;
+            capacity = 0;
+        }
+
+        public bool Exist(T element)
+        {
+            foreach (T item in array)
+            {
+                if (element.Equals(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        public void PrintArray()
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public void Reverse()
+        {
+            
+                for (int i = length-1; i >= 0; i--)
+                {
+                Console.WriteLine(array[i]);
+                }
+           
+        }
+        public void IndexOf(T element)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                if (element.Equals(array[i]))
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+            Console.WriteLine("There is no such kind of element");
+        }
+        public void LastIndexOf(T element)
+        {
+            for (int i = length - 1; i >= 0; i--)
+            {
+                if (element.Equals(array[i]))
+                {
+                    Console.WriteLine(array[i]);
+                    return;
+                }
+            }
+            Console.WriteLine("There is no such kind of element");
+        }
+
+        public void Remove()
+        {
+
+            capacity -= 1;
+
+        }
+    }
+}
---
+++ ListTask/ListTask/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ListTask;
+
+
+namespace ListTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           CreatedList<int> newList=new CreatedList<int>();
+            newList.Add(1);
+            newList.Add(2);
+            newList.Add(3);
+            newList.Add(4);
+            newList.Add(5);
+            newList.Add(6);
+
+            newList.AddItems(6, 7, 8, 9, 10);
+
+            newList.PrintArray();
+
+            newList.Reverse();
+
+            newList.PrintArray();
+
+            Console.WriteLine(newList.Exist(5));
+
+            newList.IndexOf(8);
+
+            newList.LastIndexOf(6);
+
+            newList.Clear();
+
+            newList.PrintArray();
+
+            newList.Remove();
+
+            newList.PrintArray();
+
+        }
+    }
+}
